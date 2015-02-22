a1f46d27b0fe7f0523cf390316bff1a9a5f55951
Vitaly <vbarabancev@gmail.com>
2015-02-22 07:00:33 +0600

    spelling mistake

--- src/main/java/Main/Runner.java
+++ src/main/java/Main/Runner.java
@@ -6,19 +6,18 @@ import entity.Matrix;
 public class Runner {
     public static void main(String[] args) {
         int rows = 3;
-        int collums = 3;
+        int columns = 3;
 
         Matrix matrix = new Matrix();
 
-        Matrix matrixA = new Matrix(rows, collums);
+        Matrix matrixA = new Matrix(rows, columns);
         matrixA.fillRandom();
         System.out.println("----------------");
-        Matrix matrixB = new Matrix(rows, collums);
+        Matrix matrixB = new Matrix(rows, columns);
         matrixB.fillRandom();
         System.out.println("----------------");
-        Matrix matrixResult = new Matrix(rows,collums);
-        matrixResult.fillRandom();
-        if (matrixA.getRows()==matrixB.getCollums()){
+        Matrix matrixResult = new Matrix(rows,columns);
+        if (matrixA.getRows()==matrixB.getColumns()) {
             System.out.println("Result:");
         //    matrixResult = multiply(matrixA, matrixB);
         }
@@ -32,9 +31,16 @@ public class Runner {
     private static Matrix multiply(Matrix matrixA, Matrix matrixB) {
         Matrix matrixResult = new Matrix();
         matrixResult.fillRandom();
-        matrixResult.getValue();
+        //double[][] matrix =  matrixResult.getValue();
+        int aRows = matrixA.getRows();
+        int aColumns = matrixA.getColumns();
+        int bRows = matrixB.getRows();
+        int bColumns = matrixB.getColumns();
         for (int i = 0; i < matrixResult.getValue().length; i++) {
             for (int j = 0; j < matrixResult.getValue()[i].length; j++) {
+                for (int k = 0; k < matrixResult.getValue().length; k++) {
+
+                }
 
             }
         }
